1d7218117a29479e41bd30a6970860af1d9c27c4
Edwyle Doll <edoll@mi-o6.21-school.ru>
2019-09-08 21:11:22 +0300

    upd full

--- piscine_reload/ex19/ft_sort_params.c
+++ piscine_reload/ex19/ft_sort_params.c
@@ -6,13 +6,15 @@
 /*   By: edoll <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 21:58:46 by edoll             #+#    #+#             */
-/*   Updated: 2019/09/06 22:02:52 by edoll            ###   ########.fr       */
+/*   Updated: 2019/09/08 20:55:36 by edoll            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putstr(char *str)
@@ -21,10 +23,50 @@ void	ft_putstr(char *str)
 		ft_putchar(*str++);
 }
 
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **av, int i)
+{
+	char *tmp;
+
+	tmp = av[i];
+	av[i] = av[i + 1];
+	av[i + 1] = tmp;
+}
+
 int		main(int ac, char **av)
 {
-	if (ac = 0)
-		return (0);
-	while (ac > 1)
-	{
+	int i;
 
+	if (ac > 1)
+	{
+		i = 1;
+		while (++i < ac - 1)
+		{
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+			{
+				ft_swap(av, i);
+				i = 0;
+			}
+		}
+		i = 1;
+		while (++i < ac)
+		{
+			ft_putstr(av[i]);
+			ft_putchar('\n');
+		}
+	}
+	return (0);
+}
